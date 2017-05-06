e6e9d0a574cd797902aacfb7cfd6a0865d3783fd
Joel Falknau <ozirascal@gmail.com>
2017-05-06 14:21:00 +1000

    seperated out login method, subbed settings.*** back in and add_user variables

--- services/modules/mediawiki/manager.py
+++ services/modules/mediawiki/manager.py
@@ -9,31 +9,34 @@ logger = logging.getLogger(__name__)
 
 # Activate, Create service account
 
-	@staticmethod
-	def __sanitize_username(username):
+	def sanitize_username(username):
 		sanitized = username #check mediawiki requirements, here as placeholder
 		return sanatized.lower() #but i mean who doesnt like lowering their cases
+	
+	def mediawiki_login():
+		## Login Token
+		request_login_token_data = {'action': 'query', 'format': 'json', 'meta': 'tokens', 'type': 'login'}
+		request_login_token = requests.post(settings.MEDIAWIKI_URL, data=request_login_token_data)
+		login_token = request_login_token.json()['query']['tokens']['logintoken']
+		## Login Process
+		request_login_data = {'action': 'login', 'format': 'json', 'lgname': settings.MEDIAWIKI_BOTUSERNAME, 'lgpassword': settings.MEDIAWIKI_BOTPASSWORD, 'lgtoken': login_token}
+		request_login = requests.post(settings.MEDIAWIKI_URL + 'api.php', data=request_login_data, cookies=request_login_token.cookies)
+		return request_login.cookies
 	
 	def add_user(username,password,email="",realname="", logout=True):
 	logger.debug("Adding mediawiki user with username #s, email %s, groups %s" % (
 		username, email, groups))
 		username_clean = MediawikiManager.__sanitize_username(username)
-		## Login Token
-		request_login_token_data = {'action': 'query', 'format': 'json', 'meta': 'tokens', 'type': 'login'}
-		request_login_token = requests.post('http://127.0.0.1/mediawiki/api.php', data=request_login_token_data)
-		login_token = request_login_token.json()['query']['tokens']['logintoken']
-		## Login Process
-		request_login_data = {'action': 'login', 'format': 'json', 'lgname': 'Admin@allianceauth', 'lgpassword': '3d4j5i3an2l4ci55s8qf6sca7jqbpg7m', 'lgtoken': login_token}
-		request_login = requests.post('http://127.0.0.1/mediawiki/api.php', data=request_login_data, cookies=request_login_token.cookies)
 		## Create Token
 		request_create_token_data = {'action': 'query', 'format': 'json', 'meta': 'tokens', 'type': 'createaccount'}
-		request_create_token = requests.post('http://127.0.0.1/mediawiki/api.php', data=request_create_token_data, cookies=request_login.cookies)
+		login_cookie = mediawiki_login()
+		request_create_token = requests.post(settings.MEDIAWIKI_URL + 'api.php', data=request_create_token_data, cookies=login_cookie)
 		create_token = request_create_token.json()['query']['tokens']['createaccounttoken']
 		## Build Change Cookie
-		create_cookie = request_login.cookies.copy()
+		create_cookie = login_cookie.copy()
 		create_cookie.update(request_create_token.cookies)
 		## Apply User Change
-		request_adduser_data = {'action': 'createaccount','format': 'json', 'username': 'testuser2','password': 'password','retype': 'password','email': 'test@test.com','realname': 'realname','createreturnurl': 'http://example.com','createtoken': create_token}
+		request_adduser_data = {'action': 'createaccount','format': 'json', 'username': username,'password': password,'retype': password,'email': email,'realname': realname,'createreturnurl': settings.MEDIAWIKI_URL,'createtoken': create_token}
 		if response['createaccout']['status'] = 'FAIL'
 			return 'FAIL'
 		else
