6a918c177e2dd243220e7dcb28bba6740ce3c9b1
Joel Falknau <ozirascal@gmail.com>
2017-05-07 16:45:19 +1000

    add group manager

--- services/modules/mediawiki/manager.py
+++ services/modules/mediawiki/manager.py
@@ -1,9 +1,10 @@
 import random
 import string
 import requests
-import logging
+
 from django.conf import settings
 
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -28,8 +29,8 @@ logger = logging.getLogger(__name__)
 		return request_login.cookies
 	
 	def add_user(username,password,email="",realname="", logout=True):
-	logger.debug("Adding mediawiki user with username #s, email %s, groups %s" % (
-		username, email, groups))
+	logger.debug("Adding mediawiki user with username #s, email %s" % (
+		username, email))
 		username_clean = sanitize_username(username)
 		## Create Token
 		request_create_token_data = {'action': 'query', 'format': 'json', 'meta': 'tokens', 'type': 'createaccount'}
@@ -42,12 +43,10 @@ logger = logging.getLogger(__name__)
 		## Apply User Change
 		request_adduser_data = {'action': 'createaccount','format': 'json', 'username': username_clean,'password': password,'retype': password,'email': email,'realname': realname,'createreturnurl': settings.MEDIAWIKI_URL,'createtoken': create_token}
 		response = requests.post(settings.MEDIAWIKI_URL, data=request_adduser_data,cookies=create_cookie)
-		if response['createaccout']['status'] = 'FAIL'
+		if response.json()['createaccount']['status'] = 'FAIL'
 			return 'FAIL'
 		else
 			return 'PASS'
-
-# Deactivate, delete service account i think this is block on mediawiki?
 
 	def disable_user(username,password)
 		## CSRF Token
@@ -62,8 +61,23 @@ logger = logging.getLogger(__name__)
 		request_blockuser_data = {'action': 'block', 'format': 'json', 'user': username, 'expiry': 'never', 'reason': 'AA Disable Account', 'token': csrf_token}
 		response = requests.post(settings.MEDIAWIKI_URL, data=request_blockuser_data,cookies=csrf_cookie)
 		
-	def delete_user
+	def user_group_management(username,addgroup,removegroup)
+		## userrights Token
+		request_userrights_token_data = {'action': 'query', 'format': 'json', 'meta': 'tokens', 'type': 'userrights'}
+		login_cookie = mediawiki_login()
+		request_userrights_token = requests.post(settings.MEDIAWIKI_URL + 'api.php', data=request_userrights_token_data, cookies=login_cookie)
+		userrights_token = request_csrf_token.json()['query']['tokens']['userrightstoken']
+		## Build userrights Cookie
+		userrights_cookie = login_cookie.copy()
+		userrights_cookie.update(request_userrights_token.cookies)
+		## Add user to group
+		## NOTE NOTE add group and remove group need to be | seperated lists, probably make sure of that here???
+		## Also note, these fail silently, so check for group response??
+		## im thinking this can be called one or two at a time, a bulk update would remove all invalid, add all valid as a double check
+		request_groupmanagement_data = {'action': 'userrights', 'format': 'json', 'user': username, 'add': addgroup,'remove': removegroup, 'reason': 'AA Manage Groups', 'token': userrights_token}
+		response = requests.post(settings.MEDIAWIKI_URL, data=request_groupmanagement_data,cookies=userrights_cookie)
 
 # Reset Password
+# send an email? but we can capture temp and return to view 
 
-# Set Password
+# Set Password, Not supported
